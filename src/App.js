70036dbff1cb16a8c6c447edb95aaf0b70da2327
meromantic129 <meromantic129@gmail.com>
2025-06-09 15:54:16 +0200

    update the home route

--- src/App.js
+++ src/App.js
@@ -115,7 +115,7 @@ function App() {
         <Route path="*" element={<NoMatch />}></Route>
         <Route path="/reactflow" element={<Reactflow />}></Route>
         <Route>
-          <Route path="/home" element={<Home />}>
+          <Route path="/" element={<Home />}>
             <Route path="about" element={<About />}></Route>
           </Route>
         </Route>
